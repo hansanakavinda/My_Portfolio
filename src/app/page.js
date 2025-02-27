8066431d25fd91be7507f3f321037b0b7dd3fdba
Hansana Kavinda <hansanakavinda7@gmail.com>
2025-02-27 13:42:59 +0530

    testing color changes on mobile

--- src/app/page.js
+++ src/app/page.js
@@ -25,15 +25,15 @@ export default function Home() {
   return (
     <div>
       {/* Home Section */}
-      <section id="home" className="h-screen flex flex-col md:flex-row items-center justify-center bg-white/0 leading-relaxed relative px-8 text-center md:text-left">
-  <div className="flex flex-col items-center md:items-start pr-8">
+      <section id="home" className="h-screen flex flex-col md:flex-row items-center justify-center bg-white/0 leading-relaxed relative px-8 text-center md:text-left gap-4 ">
+  <div className="flex flex-col items-center md:items-start ">
     <p className="text-3xl md:text-5xl lg:text-6xl font-bold bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">
       Hi, I'm
     </p>
     <h1 className="text-4xl md:text-6xl lg:text-8xl font-raj font-bold mt-6 tracking-widest text-white-300 neon-text futuristic-text-glitch">
       Hansana Kavinda
     </h1>
-    <p className="text-2xl md:text-4xl lg:text-5xl font-bold mt-6 tracking-widest bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent h-20">
+    <p className="text-2xl md:text-4xl lg:text-5xl font-bold mt-6 tracking-widest bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent h-20 ">
       <Typewriter
         words={["A Software Engineer", "A Web Developer"]}
         loop={0}
@@ -46,7 +46,7 @@ export default function Home() {
     </p>
   </div>
   
-  <div className="flex flex-col items-center justify-center mt-10 md:mt-0">
+  <div className="flex flex-col items-center justify-center md:mt-0">
     <div className="relative w-40 h-40 md:w-60 md:h-60 lg:w-[350px] lg:h-[350px] rounded-full bg-radial">
       <div className="w-full h-full bg-black/20 rounded-full flex items-center justify-center">
         <Image
